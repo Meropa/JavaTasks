fb542ef6d9e46676772f5568e8ce9f8c0cd921ea
Meropa <zlata.lashkevich@ya.ru>
2015-09-30 02:15:47 +0300

    main function is written

---
+++ Task1/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int len = args.length;
+		double arr[] = new double[len];
+		for (int i = 0; i < len; i++) {
+			arr[i] = Double.parseDouble(args[i]);
+		}
+		
+		//call of sort function
+		
+		printArray(arr);
+	}
+
+	private static void printArray(double[] arr) {
+		int len = arr.length;
+		for (int i = 0; i < len; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+}
