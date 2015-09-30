8e2d512f1d289e61a7871a0dfc228c44e1e5d0e3
Meropa <zlata.lashkevich@ya.ru>
2015-09-30 03:56:32 +0300

    bug is fixed

--- Task1/src/BubbleSort.java
+++ Task1/src/BubbleSort.java
@@ -1,16 +1,12 @@
 
 public class BubbleSort {
-
-	public static void main() {
-		System.out.println("Hello!");
-	}
 	
 	public static void sort(double[] arr) {
 		int len = arr.length;
 		for (int i = 0; i < len - 1; i++) {
 			for (int j = 0; j < len - i - 1; j++) {
 				if (arr[j] > arr[j+1]) {
-					swap(arr, i, j);
+					swap(arr, j, j + 1);
 				}
 			}
 		}
