890f3e0582bec4d422bbe9f7d3bc7cc7b4f3bd4d
Meropa <zlata.lashkevich@ya.ru>
2015-10-02 11:36:49 +0300

    spaces instead of tabs

--- Task1/src/BubbleSort.java
+++ Task1/src/BubbleSort.java
@@ -1,21 +1,21 @@
 
 public class BubbleSort {
-	
-	public static void sort(double[] arr) {
-		int len = arr.length;
-		for (int i = 0; i < len - 1; i++) {
-			for (int j = 0; j < len - i - 1; j++) {
-				if (arr[j] > arr[j+1]) {
-					swap(arr, j, j + 1);
-				}
-			}
-		}
-	}
-	
-	private static void swap(double[] arr, int i, int j) {
-		double c = arr[i];
-		arr[i] = arr[j];
-		arr[j] = c;
-	}
+
+    public static void sort(double[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    private static void swap(double[] arr, int i, int j) {
+        double c = arr[i];
+        arr[i] = arr[j];
+        arr[j] = c;
+    }
 
 }
